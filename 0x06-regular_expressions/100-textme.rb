9503080afa81660455a181961e731519c191f331
Martins Ndifon <martinsndifon@gmail.com>
2023-01-31 14:57:16 +0000

    update to advance task 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 # Outputs sender's and receiver's information and flags from test messages
 
-print ARGV[0].scan(/from:(\+?[a-zA-Z0-9]*)/).join
+puts ARGV[0].scan(/from:(\+?[a-zA-Z0-9]*)/).join
 print ","
-print ARGV[0].scan(/to:(\+?\w*)/).join # \w is also the character range [A-Za-z0-9_]
+puts ARGV[0].scan(/to:(\+?\w*)/).join # \w is also the character range [A-Za-z0-9_]
 print ","
-print ARGV[0].scan(/flags:(\+?[:\-0-9]*)/).join
+puts ARGV[0].scan(/flags:(\+?[:\-0-9]*)/).join
 print "\n"
