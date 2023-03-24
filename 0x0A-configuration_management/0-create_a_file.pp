fe258fa10ff2ac01faac5d78bb1cd051eb47554f
Martins Ndifon <martinsndifon@gmail.com>
2023-03-24 13:17:44 +0000

    update to task 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
 # create a file with permissions using puppet
-node default{
-file{'/tmp/school':
-mode    => '0744'
-owner   => 'www-data'
-group   => 'www-data'
-content => 'I love Puppet'}}
+
+file{'create file':
+path    => '/tmp/school',
+mode    => '0744',
+owner   => www-data,
+group   => www-data,
+content => 'I love Puppet'
+}
