9922ed19ac2a8c9cbe62fa3899225788cf15f028
Martins Ndifon <martinsndifon@gmail.com>
2023-04-24 22:47:57 +0000

    update to task 3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Uses a REST API to get and return some information"""
+import requests
+import json
+
+
+def main():
+    """Save all information about a user's tasks to a json file"""
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    users = requests.get(url)
+
+    if users.status_code == 200:
+        with open("todo_all_employees.json", 'w') as json_file:
+            objs = {}
+            for user in users.json():
+                todo_url = '{}{}/todos'.format(url, user['id'])
+                todo = requests.get(todo_url)
+                name = user['username']
+                obj_props = []
+                user_id = ''
+                for task in todo.json():
+                    obj = {
+                            "task": task["title"],
+                            "completed": task["completed"],
+                            "username": name}
+                    user_id = task["userId"]
+                    obj_props.append(obj)
+                objs[user_id] = obj_props
+            json.dump(objs, json_file)
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
