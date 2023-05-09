600673bf526d83d18dea6d53897ae3793308adc3
Martins Ndifon <martinsndifon@gmail.com>
2023-05-09 12:46:45 +0000

    update to task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,18 +2,18 @@
 """Advanced API module using reddit api endpoint"""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Request reddit api and return subreddit count"""
     url = "https://www.reddit.com/r/{}.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    subscribers = 0
+    num_subs = 0
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         data = response.json()
 
-        subscribers = data["data"]["children"][1]["data"]["subreddit_subscribers"]
-        return subscribers
+        num_subs = data["data"]["children"][1]["data"]["subreddit_subscribers"]
+        return num_subs
     except requests.exceptions.HTTPError as error:
-        return subscribers
-
+        return num_subs
