9090c58ff21b0d152cab221eb683edec86baaf3e
Martins Ndifon <martinsndifon@gmail.com>
2023-05-10 16:37:22 +0000

    update to task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Configure server and make corrections to typo error
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
+}
